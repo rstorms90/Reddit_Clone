dcb5f61d8ab0a3fbe2a34945b28ba6cbcef551fe
Russ Storms <rstorms90@gmail.com>
2021-01-03 17:10:51 -0700

    Starting globalerror handling

---
+++ web/src/pages/create-post.tsx
@@ -0,0 +1,53 @@
+import { Box, Button } from '@chakra-ui/core';
+import { Formik, Form } from 'formik';
+import React from 'react';
+import { InputField } from '../components/InputField';
+import { Layout } from '../components/Layout';
+import { useCreatePostMutation } from '../generated/graphql';
+import { useRouter } from 'next/router';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+
+const CreatePost: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const [, createPost] = useCreatePostMutation();
+
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: '', text: '' }}
+        onSubmit={async (values) => {
+          const { error } = await createPost({ input: values });
+          if (error) {
+          }
+
+          router.push('/');
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" placeholder="title" label="title" />
+            <Box mt={4}>
+              <InputField
+                textarea
+                name="text"
+                placeholder="text..."
+                label="Body"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              variantColor="teal"
+            >
+              Create post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(CreatePost);
